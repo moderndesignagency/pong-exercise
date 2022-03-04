c6c21387bad181677a3c289c7358e1ffd06e764e
Wilson Gouanet <wilstito@gmail.com>
2022-03-04 17:56:08 -0500

    Add websocket reconnection

--- client/src/store/game.js
+++ client/src/store/game.js
@@ -1,5 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import GameState from '../enums/game-state'
+import Sockette from 'sockette'
+import { createSlice } from '@reduxjs/toolkit'
+import OnlineStatus from '../enums/online-status'
 
 export const gameApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
@@ -8,32 +11,35 @@ export const gameApi = createApi({
       query: channel => channel,
       async onCacheEntryAdded(
         arg,
-        { updateCachedData, cacheDataLoaded, cacheEntryRemoved, getCacheEntry }
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved, getCacheEntry, dispatch }
       ) {
-        // create a websocket connection when the cache subscription starts
-        const ws = new WebSocket('ws://localhost:8080/ws')
+        let isCachedDataLoaded = false
+        const listener = event => {
+          const data = JSON.parse(event.data)
+          const cachedEntry = getCacheEntry()
+          if (
+            !isCachedDataLoaded ||
+            (data.state !== GameState.PLAY && data.state === cachedEntry.data?.state)
+          ) {
+            return
+          }
+
+          updateCachedData(() => {
+            return data
+          })
+        }
+        const ws = new Sockette('ws://localhost:8080/ws', {
+          maxAttemps: 50,
+          timeout: 5000,
+          onmessage: listener,
+          onopen: () => dispatch(updateOnlineStatus({ status: OnlineStatus.ONLINE })),
+          onreconnect: () => dispatch(updateOnlineStatus({ status: OnlineStatus.RECONNECTING })),
+          onerror: () => dispatch(updateOnlineStatus({ status: OnlineStatus.OFFLINE })),
+        })
         try {
           // wait for the initial query to resolve before proceeding
           await cacheDataLoaded
-
-          // when data is received from the socket connection to the server,
-          // if it is a message and for the appropriate channel,
-          // update our query result with the received message
-          const listener = event => {
-            const data = JSON.parse(event.data)
-            // console.log(data)
-            const cachedEntry = getCacheEntry()
-            if (data.state !== GameState.PLAY && data.state === cachedEntry.data?.state) {
-              return
-            }
-            // console.log(cachedEntry);
-
-            updateCachedData(() => {
-              return data
-            })
-          }
-
-          ws.addEventListener('message', listener)
+          isCachedDataLoaded = true
         } catch {
           // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
           // in which case `cacheDataLoaded` will throw
@@ -48,4 +54,19 @@ export const gameApi = createApi({
   }),
 })
 
+export const gameSlice = createSlice({
+  name: 'game',
+  initialState: {
+    onlineStatus: OnlineStatus.CONNECTING,
+  },
+  reducers: {
+    updateOnlineStatus(state, { payload }) {
+      const { status } = payload
+      state.onlineStatus = status
+    },
+  },
+})
+
 export const { useGetGameStateQuery } = gameApi
+
+export const { updateOnlineStatus } = gameSlice.actions
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
-import { gameApi } from './game'
+import { gameApi, gameSlice } from './game'
 
 export const store = configureStore({
   reducer: {
     [gameApi.reducerPath]: gameApi.reducer,
+    [gameSlice.name]: gameSlice.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(gameApi.middleware),
 })
