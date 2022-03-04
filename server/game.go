370256728bb91d87b0c6fd2fb8d4d37d427127ab
Wilson Gouanet <wilstito@gmail.com>
2022-03-04 14:10:52 -0500

    export game level field in json

--- server/game.go
+++ server/game.go
@@ -15,9 +15,9 @@ type Game struct {
 	Player1     *pong.Paddle `json:"player1"`
 	Player2     *pong.Paddle `json:"player2"`
 	Screen      *pong.Screen `json:"screen"`
+	Level       int          `json:"level"`
 	color.Color `json:"color"`
 	rally       int
-	level       int
 	maxScore    int
 }
 
@@ -40,6 +40,8 @@ const (
 	InitPaddleShift  = 50
 )
 
+var whiteColor = color.RGBA{255, 255, 255, 255}
+
 // NewGame creates an initializes a new game
 func NewGame() *Game {
 	g := &Game{}
@@ -60,18 +62,18 @@ func (g *Game) Init() {
 		Speed:  initPaddleSpeed,
 		Width:  InitPaddleWidth,
 		Height: InitPaddleHeight,
-		Color:  color.White,
+		Color:  whiteColor,
 		Up:     pong.KeyW,
 		Down:   pong.KeyS,
 	}
 	g.Player2 = &pong.Paddle{
 		X:      windowWidth - InitPaddleShift - InitPaddleWidth,
 		Y:      (windowHeight - InitPaddleHeight) / 2,
-		Score:  0,
+		Score:  2,
 		Speed:  initPaddleSpeed,
 		Width:  InitPaddleWidth,
 		Height: InitPaddleHeight,
-		Color:  color.White,
+		Color:  whiteColor,
 		Up:     pong.KeyUp,
 		Down:   pong.KeyDown,
 	}
@@ -79,18 +81,18 @@ func (g *Game) Init() {
 		Cx:     float32(windowWidth / 2),
 		Cy:     float32(windowHeight / 2),
 		Radius: InitBallRadius,
-		Color:  color.White,
+		Color:  color.RGBA{255, 255, 255, 200},
 		Vx:     initBallVelocity,
 		Vy:     -initBallVelocity,
 	}
-	g.level = 0
+	g.Level = 0
 }
 
 func (g *Game) reset(state GameState) {
 	w, h := g.Screen.Size()
 	g.State = state
 	g.rally = 0
-	g.level = 0
+	g.Level = 0
 	if state == StartState {
 		g.Player1.Score = 0
 		g.Player2.Score = 0
@@ -120,8 +122,11 @@ func (g *Game) Update() {
 	case PlayState:
 		w, _ := g.Screen.Size()
 
-		g.Player1.Update(g.Screen)
-		g.Player2.Update(g.Screen)
+		// g.Player1.Update(g.Screen)
+		// g.Player2.Update(g.Screen)
+		// @FixMe: remove the code above
+		g.Player1.AiUpdate(g.Screen, g.Ball)
+		g.Player2.AiUpdate(g.Screen, g.Ball)
 
 		prevVx := g.Ball.Vx
 		g.Ball.Update(g.Player1, g.Player2, g.Screen)
@@ -129,7 +134,7 @@ func (g *Game) Update() {
 		if prevVx*g.Ball.Vx < 0 {
 			g.rally += 1
 			if (g.rally)%speedUpdateCount == 0 {
-				g.level++
+				g.Level++
 				g.Ball.Vx += speedIncrement
 				g.Ball.Vy += speedIncrement
 				g.Player1.Speed += speedIncrement
@@ -152,7 +157,8 @@ func (g *Game) Update() {
 }
 
 func (g *Game) Start(hub *Hub) {
-	ticker := time.NewTicker(1000 * time.Millisecond / 50)
+	// 50 FPS
+	ticker := time.NewTicker(time.Second / 50)
 
 	go func() {
 		for {
@@ -166,7 +172,8 @@ func (g *Game) Start(hub *Hub) {
 		}
 	}()
 
-	time.Sleep(30000 * time.Millisecond)
+	// @FixMe: find a better solution
+	time.Sleep(1000 * time.Hour)
 
 	defer ticker.Stop()
 }
