b3937fcd5553981a63d1e86f52f6835ffddd5cb3
Wilson Gouanet <wilstito@gmail.com>
2022-03-05 02:25:40 -0500

    Add draw start and game over states

--- client/src/drawer.js
+++ client/src/drawer.js
@@ -5,12 +5,19 @@ export function setupCanvas(canvas) {
   canvas.setAttribute('height', '600')
 }
 
+/**
+ * Draws the current game state on the canvas
+ * @param {HTMLCanvasElement} canvas 
+ * @param {*} gameState The current game state
+ */
 export function draw(canvas, gameState) {
-  const ctx = canvas.getContext('2d')
-
-  if (gameState.state !== GameState.PLAY) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  if(gameState.state === GameState.OVER) {
+    return drawGameOver(canvas, gameState)
+  } else if (gameState.state === GameState.START) {
+    return drawGameStart(canvas, gameState)
   }
+
+  const ctx = canvas.getContext('2d')
   drawRect(ctx, 0, 0, canvas.width, canvas.height, parseColor(gameState.color))
 
   // Drawing ball
@@ -42,7 +49,7 @@ export function draw(canvas, gameState) {
 
   // Score
   ctx.textAlign = 'center'
-  ctx.font = '200px Arial'
+  ctx.font = 'bold 200px Arial'
   ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
   ctx.fillText(gameState.player1.score, 170, 300)
   ctx.fillText(gameState.player2.score, canvas.width - 190, 300)
@@ -73,4 +80,47 @@ function drawCircle(ctx, cx, cy, radius, color) {
 
 function parseColor(color) {
   return `rgba(${color.R}, ${color.G}, ${color.B}, ${color.A / 255})`
+}
+
+function drawGameStart(canvas, gameState) {
+  const ctx = canvas.getContext('2d')
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawRect(ctx, 0, 0, canvas.width, canvas.height, parseColor(gameState.color))
+  ctx.textAlign = 'center'
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.7)'
+  ctx.strokeStyle = 'white'
+  ctx.lineWidth = 3
+
+  ctx.font = 'bold 90px Arial'
+  ctx.strokeText('⚡PONG GAME⚡', canvas.width / 2, 200)
+
+  ctx.font = '50px Arial'
+  ctx.fillText('Hit Space to start a new Game', canvas.width / 2, 300)
+}
+
+function drawGameOver(canvas, gameState) {
+  const ctx = canvas.getContext('2d')
+  const middleWidth = canvas.width / 2
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawRect(ctx, 0, 0, canvas.width, canvas.height, parseColor(gameState.color))
+  ctx.textAlign = 'center'
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.7)'
+  ctx.strokeStyle = 'white'
+  ctx.lineWidth = 3
+
+  ctx.font = '20px Arial'
+  ctx.fillText(`Level: ${gameState.level}`, middleWidth, 30)
+
+  ctx.font = 'bold 100px Arial'
+  ctx.strokeText('GAME OVER', middleWidth, 200)
+
+  ctx.font = 'bold 70px Arial'
+  ctx.fillText(
+    `✨ PLAYER ${gameState.player1.score > gameState.player2.score ? 1 : 2} Won ✨`,
+    middleWidth,
+    320
+  )
+
+  ctx.font = '50px Arial'
+  ctx.fillText('Hit Space to start a new Game', middleWidth, 400)
 }
