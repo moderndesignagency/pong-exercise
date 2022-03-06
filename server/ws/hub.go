30ff60cc93ea6bc01cefc7389c8d48b35192639d
Wilson Gouanet <wilstito@gmail.com>
2022-03-05 21:15:00 -0500

    move client and websocket into the ws package

---
+++ server/ws/hub.go
@@ -0,0 +1,63 @@
+package ws
+
+import (
+	"log"
+	"time"
+
+	"github.com/moderndesignagency/pong-exercise/server/input"
+)
+
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	Broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+
+	// Key pressed input from clients.
+	input chan []byte
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		Broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+		input:      make(chan []byte),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.Broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		case i := <-h.input:
+			log.Println(input.Key(i))
+			input.AddPressedKey(input.Key(i), time.Now())
+		}
+	}
+}
