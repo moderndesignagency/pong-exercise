cd50ad26d934e222515c04e94b2fcb7dc7ef4605
Wilson Gouanet <wilstito@gmail.com>
2022-03-11 17:53:30 -0500

    Add new colors

--- server/pong/utils.go
+++ server/pong/utils.go
@@ -7,11 +7,17 @@ import (
 )
 
 var foregroundColors = []color.Color{
-	color.RGBA{073, 126, 118, 200},
-	color.RGBA{100, 036, 036, 200},
-	color.RGBA{034, 113, 179, 200},
+	color.RGBA{73, 126, 118, 200},
+	color.RGBA{150, 36, 36, 200},
+	color.RGBA{34, 113, 179, 200},
 	color.RGBA{222, 076, 138, 200},
-	color.RGBA{0, 0, 0, 200},
+	color.RGBA{20, 20, 30, 200},
+	color.RGBA{205, 102, 113, 200},
+	color.RGBA{205, 160, 122, 200},
+	color.RGBA{32, 178, 170, 200},
+	color.RGBA{105, 106, 250, 200},
+	color.RGBA{128, 0, 128, 200},
+	color.RGBA{150, 150, 0, 200},
 }
 
 func RandomColor() color.Color {
