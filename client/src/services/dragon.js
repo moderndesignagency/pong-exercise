b8f7ddc36943970db0e0bc654730f8e746929178
Wilson Gouanet <wilstito@gmail.com>
2022-03-23 03:38:18 -0400

    Update dragon fire parameters

--- client/src/services/dragon.js
+++ client/src/services/dragon.js
@@ -30,16 +30,18 @@ function createImageEmitter({ canvas, x, y, startColor, endColor }) {
   emitter.rate = new Proton.Rate(new Proton.Span(5, 7), new Proton.Span(0.01, 0.02))
 
   emitter.addInitialize(new Proton.Mass(1))
-  emitter.addInitialize(new Proton.Life(1))
-  emitter.addInitialize(new Proton.Body([dot], 16))
+  emitter.addInitialize(new Proton.Life(0.6))
+  emitter.addInitialize(new Proton.Body([dot], 36))
   emitter.addInitialize(new Proton.Radius(10))
 
   emitter.addBehaviour(new Proton.Alpha(1, 0))
   emitter.addBehaviour(new Proton.Color(startColor, endColor))
-  emitter.addBehaviour(new Proton.Scale(2, 0.5))
+  emitter.addBehaviour(new Proton.Scale(2))
   emitter.addBehaviour(
     new Proton.CrossZone(new Proton.RectZone(0, 0, canvas.width, canvas.height), 'dead')
   )
+  emitter.addBehaviour(new Proton.Rotate(0, Proton.getSpan(-8, 9), "add"));
+  emitter.addBehaviour(new Proton.RandomDrift(3, 3, 0.03));
 
   emitter.p.x = x
   emitter.p.y = y
